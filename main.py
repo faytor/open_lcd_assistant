3e42d24f7cde89e796fd50949ddd12e593e4c470
Fay <fay.tor@gmail.com>
2021-08-05 20:02:25 +0200

    Add convert horizontal

--- main.py
+++ main.py
@@ -77,7 +77,8 @@ class MyApp(QtWidgets.QMainWindow, Ui_MainWindow):
             self.__convert_vertical()
             self.save_button.setEnabled(True)
         if horizontal:
-            print("horizontal")
+            self.__convert_horizontal()
+            self.save_button.setEnabled(True)
 
 
     def save_bmp_txt(self):
@@ -106,6 +107,7 @@ class MyApp(QtWidgets.QMainWindow, Ui_MainWindow):
             for x in range(self.__image_height):
                 one_byte = pad_array[y:y+8,x]
                 a = (''.join(str(y) for y in np.flip(one_byte)))
+                #a = (''.join(str(y) for y in one_byte))
                 byte_array += '0x'+format((int(a, 2)), '02x')+','
                 index += 1
                 if index%16 == 0:
@@ -117,7 +119,22 @@ class MyApp(QtWidgets.QMainWindow, Ui_MainWindow):
         return byte_array 
 
     def __convert_horizontal(self):
-        pass
+        byte_array = ''
+        index = 0
+        pad_array = self.__format_image()
+        for y in range(self.__image_height):
+            for x in range(0,self.__image_width,EIGHT_BIT):
+                one_byte = pad_array[x:x+EIGHT_BIT,y]
+                a = (''.join(str(i) for i in np.flip(one_byte)))
+                #a = (''.join(str(i) for i in one_byte))
+                byte_array += '0x'+format((int(a, 2)), '02x')+','
+                index += 1
+                if index%16 == 0:
+                    byte_array += '\n'
+                
+        byte_array = '{\n' + byte_array.strip()[:-1] + '};' # remove the last comma  
+        self.plainTextEdit.setPlainText(byte_array)
+        return byte_array 
 
 
 
